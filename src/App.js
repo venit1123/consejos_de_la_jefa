1e75619d5147f1bcde08f4f55ae4936a5a225dfc
Itzel Ventura <itzel@Itzels-MacBook-Pro.local>
2023-01-05 21:14:42 -0800

    Adding costume hook to set and clear to text fields

--- src/App.js
+++ src/App.js
@@ -4,39 +4,54 @@ import { useState, Fragment } from 'react';
 
 function App({}) {
 
-  const [recipeName, setRecipeName] = useState('')
-  const [authorName, setAuthorName] = useState('')
+  const [recipeTitleProps, resetRecipeTitle] = useInput('')
+  const [authorNameProps, resetAuthorName] = useInput('')
   const [showRecipeInput, setShowRecipeInput] = useState('visible')
   const [isShown, setIsShown] = useState(true);
 
-  const handleRecipeNameChange = (event) => {
-    setRecipeName(event.target.value);
-  };
 
   const submit = (e) => {
     e.preventDefault();
+    alert(`${recipeTitleProps.value} ${authorNameProps.value}`)
     setShowRecipeInput('hidden');
     setIsShown(false);
+    // resetRecipeTitle()
+    // resetAuthorName()
   };
 
-  function RecipeHeader({currentRecipeName}) {
+  function RecipeHeader() {
     return(
       <Fragment>
-        <h1>Receta para hacer {currentRecipeName} </h1>
-        <h3>Autor: {authorName}</h3>
+        <h1>Receta para hacer {recipeTitleProps.value} </h1>
+        <h3>Autor: {authorNameProps.value} </h3>
       </Fragment>
     );
   }
   
+  function useInput(initialValue) {
+    const [value, setValue] = useState(initialValue)
+    return [
+      {
+        value,
+        onChange: (event) => { setValue(event.target.value) }
+      },
+      () => setValue(initialValue)
+    ]
+  };
   return (
     <div className="App">
       <form onSubmit={submit}>
         <input
+          {...recipeTitleProps}
           style={{visibility: showRecipeInput}}
-          value={recipeName}
           type="text"
           placeholder="Nombre de el Platillo" 
-          onChange={handleRecipeNameChange}
+        />
+        <input
+          {...authorNameProps}
+          style={{visibility: showRecipeInput}}
+          type="text"
+          placeholder="autor" 
         />
         <br></br>
         <div>
@@ -44,7 +59,7 @@ function App({}) {
         </div>
       </form>
 
-      <RecipeHeader currentRecipeName={recipeName}/>
+      <RecipeHeader/>
       
     </div>
   );
