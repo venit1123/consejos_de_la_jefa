e28cd2b3ae32dac1f0083e70e2f0b5590dcd45e6
Itzel Ventura <itzel@Itzels-MacBook-Pro.local>
2023-01-10 15:17:38 -0800

    Implementing testing App component using react testing libraly

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByText } = render(<App/>);
+  const h1 = getByText(/This is the App container/);
+  expect(h1).toHaveTextContent("This is the App container")
 });
