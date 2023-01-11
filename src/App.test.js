409e64e5a59e7dca7760d367316007c1b66b2f7b
Itzel Ventura <itzel@Itzels-MacBook-Pro.local>
2023-01-10 16:00:18 -0800

    Adding test events, example 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App/>);
+//   const h1 = getByText(/This is the App container/);
+//   expect(h1).toHaveTextContent("This is the App container")
+// });
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App/>);
-  const h1 = getByText(/This is the App container/);
-  expect(h1).toHaveTextContent("This is the App container")
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
