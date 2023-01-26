e34b12ff7c7a666437242d19255bf2e5ea2f77e2
Itzel Ventura <itzel@Itzels-MacBook-Pro.local>
2023-01-26 14:35:39 -0800

    Deleted unused files

--- src/Checkedbox.test.js
+++
@@ -1,9 +0,0 @@
-// import { fireEvent, render } from '@testing-library/react';
-// import Checkbox from "./Checkbox"
-
-// test("Selecting the checkbox should change value to True", () => {
-//     const { getByLabelText } = render(<Checkbox/>)
-//     const checkbox = getByLabelText(/not checked/i) //regex, the "i" used to specify that is non-case sensitive
-//     fireEvent.click(checkbox);
-//     expect(checkbox.checked).toEqual(true)
-// })
